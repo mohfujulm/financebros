8ce12c8456c695f9d3dbb698c6a004b1979bcaeb
Mohfujul Mohammed <mohfuj234@gmail.com>
2023-02-20 11:33:48 -0500

    some more testing with firebase data retrieval

--- src/screens/Starter.tsx
+++ src/screens/Starter.tsx
@@ -11,7 +11,7 @@ export default function Starter(): JSX.Element{
   const [users, setUsers] = useState([]); // Initial empty array of users
 
   //sample data retrieval with firebase shown below:
-  //accesses the firestore database, finds the collection named 'users', and pulls every document in the collection and stores it in the users array as JSON
+  //accesses the firestore database, finds the collection named 'users', and pulls every document in the collection and stores it in the 'users' array as JSON
   useEffect(() => {
     const subscriber = firebase.firestore()
       .collection('users')
@@ -33,10 +33,24 @@ export default function Starter(): JSX.Element{
   }, []);
 
     //press the button below to display console log of the data pulled
+    //the <text> block shows how to access the data pulled from Firestore
     return (
       <View>
         <View style={styles.MainContainer}>
-          <Button title = "press me to display data in console" onPress = {() => console.log(users)}/>
+          
+          
+          <Text style = {styles.sectionTitle}>Information from first two entries of the Firestore database: </Text> 
+
+          <Text style = {styles.bodyText}>{'\n'} [ User: { users[0].username }, Pass: { users[0].password } ] </Text>
+          <Text style = {styles.bodyText}>{'\n'} [ User: { users[1].username }, Pass: { users[1].password } ] </Text>
+
+          <Pressable onPress = {() => console.log(users)}
+                     style = {styles.button}>
+                     
+                     <Text style = {styles.buttonText}>Press me to display contents of the 'users' collection from Firestore in the console</Text>
+
+          </Pressable>
+
         </View>
     </View>
     );
@@ -48,5 +62,33 @@ const styles = StyleSheet.create({
     height: '100%',
     width: '100%',
     backgroundColor: 'pink',
+    alignItems: 'center',
   },
+
+  sectionTitle: {
+    marginTop: 20,
+    fontSize: 20,
+    fontWeight: '600',
+  },
+
+  bodyText: {
+    fontSize: 16,
+  },
+
+  buttonText: {
+    fontSize: 12,
+    fontWeight: 'bold',
+  },
+
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: '8%',
+    backgroundColor: 'white',
+    width: '45%',
+    height: '7%',
+    borderRadius: 4,
+    elevation: 7
+  },
+
 });
